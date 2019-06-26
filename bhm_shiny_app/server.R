30cffbc0b3bd3bf29931065e33d38da37b5261a9
turneralex <alexanderturner1991@gmail.com>
2019-06-26 20:55:39 +1000

    add model type selection.

--- bhm_shiny_app/server.R
+++ bhm_shiny_app/server.R
@@ -1,12 +1,72 @@
 library(shiny)
 library(tidyverse)
-# library(ggmcmc)
-# library(rjags)
+library(ggmcmc)
+library(rjags)
+
+post_dist_fun <- function(x, t_p) {
+    ggplot(x, aes(value, fill = fct_inorder(parameter_fill))) +
+        geom_histogram(bins = 100, colour = "white", alpha = 0.8) +
+        geom_vline(xintercept = t_p, size = 0.8) +
+        facet_grid(Parameter ~ .) +
+        scale_fill_manual(values = RColorBrewer::brewer.pal(8, "Dark2")[c(5:6, 4)]) +
+        scale_x_continuous(limits = c(0, 1),
+                           breaks = seq(0, 1, by = 0.1)) +
+        labs(title = "Posterior Distributions",
+             x = "Value",
+             y = "Count") +
+        guides(fill = F) +
+        theme(plot.title = element_text(size = 32),
+              strip.text.y = element_text(angle = 0))
+}
+
+N <- rep(10, 10) # number of flips by coin
+n_c <- 8 # number of coins
 
 chain_length <- 100000 # chain length
 n_chains <- 3 # number of chains
 n_adapt <- 1000 # adaptation period
 burn_in <- ceiling(0.005 * chain_length) # burn in
+
+# high dependency & vague hyperprior model
+model_hd_vh <- "
+model {
+  for (c in 1:n_c) {
+    z[c] ~ dbin(theta[c], N[c])
+    theta[c] ~ dbeta(omega * (50 - 2) + 1, (1 - omega) * (50 - 2) + 1)
+  }
+  omega ~ dbeta(1, 1)
+}
+"
+
+model_ld_vh <- "
+model {
+  for (c in 1:n_c) {
+    z[c] ~ dbin(theta[c], N[c])
+    theta[c] ~ dbeta(omega * (5 - 2) + 1, (1 - omega) * (5 - 2) + 1)
+  }
+  omega ~ dbeta(1, 1)
+}
+"
+
+model_hd_ih <- "
+model {
+  for (c in 1:n_c) {
+    z[c] ~ dbin(theta[c], N[c])
+    theta[c] ~ dbeta(omega * (50 - 2) + 1, (1 - omega) * (50 - 2) + 1)
+  }
+  omega ~ dbeta(20, 20)
+}
+"
+
+model_ld_ih <- "
+model {
+  for (c in 1:n_c) {
+    z[c] ~ dbin(theta[c], N[c])
+    theta[c] ~ dbeta(omega * (5 - 2) + 1, (1 - omega) * (5 - 2) + 1)
+  }
+  omega ~ dbeta(20, 20)
+}
+"
 
 shinyServer(function(input, output) {
 
@@ -26,6 +86,58 @@ shinyServer(function(input, output) {
         temp_df
     })
     
+    jags_data <- eventReactive(input$run_jags, {
+        z <- coins_df() %>%  
+            group_by(coin) %>% 
+            summarise(z = (value == "H") %>% sum()) %>% 
+            pull(z)
+        
+        data_list <- list(
+            N = N,
+            z = z,
+            n_c = n_c
+        )
+        
+        if (input$model_type == "High Dependency & Vague Hyperprior") {
+          writeLines(model_hd_vh, "jagsmodel.txt")
+        } else if (input$model_type == "Low Dependency & Vague Hyperprior") {
+          writeLines(model_ld_vh, "jagsmodel.txt")
+        } else if (input$model_type == "High Dependency & Informative Hyperprior") {
+          writeLines(model_hd_ih, "jagsmodel.txt")
+        } else {
+          writeLines(model_ld_ih, "jagsmodel.txt")
+        }
+        
+        # run model
+        jags_model <- jags.model(
+            file = "jagsmodel.txt",
+            data = data_list,
+            n.chains = n_chains,
+            n.adapt = n_adapt
+        )
+        
+        # update for burn in
+        update(jags_model, n.iter = burn_in)
+
+        # posterior paramaters
+        thetas <- map_chr(1:8, ~ paste0("theta[", ., "]"))
+
+        parameters <- c("omega", thetas)
+
+        # posterior samples
+        coda_samples <- coda.samples(
+            jags_model,
+            variable.names = parameters,
+            n.iter = chain_length
+        )
+
+        post_samples <- coda_samples %>%
+            ggs() %>%
+            mutate(parameter_fill = c(rep("omega", 300000), rep("theta", 2400000)) %>% factor())
+
+        post_samples
+    })
+    
     output$total_prop <- renderText({
         temp_total_prop <- coins_df() %>% 
             filter(value == "H") %>% 
@@ -36,7 +148,7 @@ shinyServer(function(input, output) {
         paste("Total proportion of Heads:", temp_total_prop)
     })
     
-    output$coin_props <- renderPlot({
+    output$coin_props_plot <- renderPlot({
         temp_total_prop <- coins_df() %>% 
             filter(value == "H") %>% 
             summarise(prop = n() / 80) 
@@ -60,12 +172,14 @@ shinyServer(function(input, output) {
         
     })
     
-    wait_text <- eventReactive(input$run_jags, {
-        "Please wait for the MCMC process to be completed."
+    output$jags_plot <- renderPlot({
+      
+        temp_total_prop <- coins_df() %>% 
+            filter(value == "H") %>% 
+            summarise(prop = n() / 80) %>% 
+            pull(prop)
+        
+        post_dist_fun(jags_data(), temp_total_prop)
     })
     
-    output$please_wait <- renderText({
-        wait_text()
-    })
-
 })
--- bhm_shiny_app/ui.R
+++ bhm_shiny_app/ui.R
@@ -6,13 +6,26 @@ shinyUI(fluidPage(
 
     sidebarLayout(
         sidebarPanel(
+            h5("Observe the effects of different levels of dependency & strength of certainity in the hyperprior
+               by choosing different parameter settings for the same data."),
+            selectInput("model_type",
+                        "Choose the model parameters:",
+                        choices = c("High Dependency & Vague Hyperprior",
+                                    "Low Dependency & Vague Hyperprior",
+                                    "High Dependency & Informative Hyperprior",
+                                    "Low Dependency & Informative Hyperprior")),
+            h5("Generate the data by flipping 8 fair coins:"),
+            h6("More extreme individual outcomes will yield more interesting modelling results."),
             actionButton("flip_coins", "Flip Coins"),
+            h5("Use JAGS to run the MCMC process:"),
+            h6("This will take ~20 seconds to complete."),
             actionButton("run_jags", "Run JAGS"),
-            verbatimTextOutput("please_wait"),
             verbatimTextOutput("total_prop"),
-            plotOutput("coin_props")
+            plotOutput("coin_props_plot")
         ),
 
-        mainPanel()
+        mainPanel(
+            plotOutput("jags_plot", height = 800)
+        )
     )
 ))
